4d261ec4a7609553d9bf8820cf78474049f0523a
Cay-y-yque <cayquecagas666@outlook.com>
2025-10-21 14:31:16 -0300

    Adição da atividade-04

---
+++ Atividades/Atividade-04/flutter_geolocation/lib/main.dart
@@ -0,0 +1,172 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+/// Determine the current position of the device.
+///
+/// When the location services are not enabled or permissions
+/// are denied the `Future` will return an error.
+Future<Position> _determinePosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  // Test if location services are enabled.
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    // Location services are not enabled don't continue
+    // accessing the position and request users of the 
+    // App to enable the location services.
+    return Future.error('Location services are disabled.');
+  }
+//a
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      // Permissions are denied, next time you could try
+      // requesting permissions again (this is also where
+      // Android's shouldShowRequestPermissionRationale 
+      // returned true. According to Android guidelines
+      // your App should show an explanatory UI now.
+      return Future.error('Location permissions are denied');
+    }
+  }
+  
+  if (permission == LocationPermission.deniedForever) {
+    // Permissions are denied forever, handle appropriately. 
+    return Future.error(
+      'Location permissions are permanently denied, we cannot request permissions.');
+  } 
+
+  // When we reach here, permissions are granted and we can
+  // continue accessing the position of the device.
+  return await Geolocator.getCurrentPosition();
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Verificando se está no IF ou não.'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final String _currentLocation = "Localização atual";
+  double? latitude;
+  double? longitude;
+  String locationMessage = "";
+  String insideIFCE = "";
+  List<double> minMaxLat = [-3.745868, -3.743229,];
+  List<double> minMaxLong = [-38.537143, -38.535511];
+  List<double> ifceCenter = [-3.744402, -38.536037];
+
+  bool _insideIFCE(){
+    if (latitude! < minMaxLat[0] || latitude! > minMaxLat[1] || longitude! < minMaxLong[0] || longitude! > minMaxLong[1]){
+      setState(() {
+        insideIFCE = "Não estás no IFCE!!!";
+      });
+      return false;
+    }
+    setState(() {
+      insideIFCE = "Você está dentro do IFCE!!!!";
+    });
+    return true;
+  }
+
+  void _liveLocation(){
+  LocationSettings locationSettings = const LocationSettings(
+    accuracy: LocationAccuracy.best,
+    distanceFilter: 1, //Taxa de atualização, atualiza a cada 1m
+  );
+
+  Geolocator.getPositionStream().listen(
+    (Position position) {
+      latitude = position.latitude;
+      longitude = position.longitude;
+      if (!_insideIFCE()){
+        String formated = Geolocator.distanceBetween(latitude!, longitude!, ifceCenter[0], ifceCenter[1]).toStringAsFixed(2);
+        insideIFCE += "Sua distância em relação ao \nIFCE: $formated metros";
+      }
+
+      setState(() {
+        locationMessage = "Latitude: $latitude Longitude $longitude";
+      });
+    }
+  );
+  }
+
+  Future<void> _openMap(double? latitude, double? longitude) async {
+    final uri = Uri.parse('https://www.google.com/maps/search/?api=1&query=$latitude,$longitude');
+
+    // tenta abrir diretamente no browser / app externo
+    if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
+      throw Exception('Não conseguiu abrir a url: $uri');
+    }
+  }
+
+  @override 
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: Text(widget.title),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(_currentLocation),
+          Text(locationMessage),
+          Text(insideIFCE),
+          ElevatedButton(
+            onPressed: () {
+              _determinePosition().then((value) {
+                latitude = value.latitude;
+                longitude = value.longitude;
+                setState(() {
+                  locationMessage = "Latitude: $latitude Longitude $longitude";
+                });
+                if (!_insideIFCE()){
+                  String formated = Geolocator.distanceBetween(latitude!, longitude!, ifceCenter[0], ifceCenter[1]).toStringAsFixed(2);
+                  insideIFCE += "Sua distância em relação ao \nIFCE: $formated metros";
+                }
+                _liveLocation();
+              });
+            }, 
+            child: Text("Pegue a sua localização atual"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              _openMap(latitude, longitude);
+            },
+            child: Text("Ver no google maps"),
+          )
+        ],
+      ) 
+      ,
+    ),
+  );
+}
+
